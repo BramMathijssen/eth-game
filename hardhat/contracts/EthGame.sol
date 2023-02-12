9625218ad7b9698ce5cb08f3465ebca8d6468c48
Bram <Bram.mathijssen@gmail.com>
2023-02-12 21:31:30 +0100

    changed natspec

--- hardhat/contracts/EthGame.sol
+++ hardhat/contracts/EthGame.sol
@@ -6,8 +6,6 @@ import "hardhat/console.sol";
 /// @title EthGame
 /// @author Bram Mathijssen
 /// @notice A game where the 14th person that deposit ether wins all the ether in the contract
-/// the winner can claim the 14 ether
-/// @dev All function calls are currently implemented without side effects
 contract EthGame {
     uint256 public s_playerCounter;
     uint256 public ETH_AMOUNT = 0.1 ether;
